b4b1d8ee85238cd994443f6bdbaada7e77fb947f
codesofbro <muhammadhanzala336@gmail.com>
2024-02-08 18:44:18 +0500

    add responsive navbar

--- index.js
+++ index.js
@@ -13,3 +13,18 @@ function addAnimation() {
       "linear-gradient(90deg, transparent, white 20%, white 80%, transparent)";
   });
 }
+document.addEventListener("DOMContentLoaded", function () {
+  const toggleNavButton = document.getElementById("toggleNav");
+  const overlay = document.getElementById("overlay");
+  const nav = document.querySelector(".nav");
+
+  toggleNavButton.addEventListener("click", function () {
+    nav.classList.toggle("active");
+    overlay.style.display = nav.classList.contains("active") ? "block" : "none";
+  });
+
+  overlay.addEventListener("click", function () {
+    nav.classList.remove("active");
+    overlay.style.display = "none";
+  });
+});
